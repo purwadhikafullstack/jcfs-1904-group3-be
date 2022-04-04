dea793b8b0a229a0d7761cf58ecaa14a9954f0da
FahemHakikiKhaya <hakikihaya@gmail.com>
2022-04-04 12:26:01 +0700

    new feature : get products based on search keyword

--- src/router/products/get.js
+++ src/router/products/get.js
@@ -5,6 +5,56 @@ const pool = require("../../config/database");
 const getProducts = router.get("/", async (req, res) => {
   try {
     const connection = await pool.promise().getConnection();
+    if (req.query.search) {
+      const getProductsByCategory = `SELECT p.id AS productId,
+      p.productName,
+      v.warehouseId,
+      v.id As variantId,
+      v.color as variant,
+      v.size,
+      v.price,
+      v.image,
+      v.qtyAvailable,
+      v.qtyTotal FROM products as p
+      JOIN variant as v ON v.productId = p.id
+      Where p.id IN(SELECT distinct p.id FROM products as p
+      JOIN categories_products as c_p ON c_p.productId = p.id 
+      JOIN categories as c ON c.id = c_p.categoryId 
+      WHERE c.categoryName LIKE ?
+      ORDER by p.id)
+      GROUP BY p.id;`;
+      const keyWord = req.query.search;
+
+      const [result] = await connection.query(getProductsByCategory, keyWord);
+      if (result.length) {
+        connection.release();
+        res.status(200).send({ result });
+      }
+      if (!result.length) {
+        const getProductByKeyword = ` SELECT p.id AS productId,
+        p.productName,
+        v.warehouseId,
+        v.id As variantId,
+        v.color as variant,
+        v.size,
+        v.price,
+        v.image,
+        v.qtyAvailable,
+        v.qtyTotal FROM products as p
+        JOIN variant as v ON v.productId = p.id
+        Where p.productName Like ? or v.color Like ? or v.size Like ?
+        GROUP BY p.id;`;
+        const data = [keyWord, keyWord, keyWord];
+        const [result] = await connection.query(getProductByKeyword, data);
+        if (!result.length) {
+          res
+            .status(400)
+            .send({ error: `Cannot find results based on the keyword` });
+        }
+        connection.release();
+        res.status(200).send({ result });
+      }
+    }
     if (req.query.category) {
       const sqlGetProductListById = `SELECT p.id AS productId,
       p.productName,
@@ -55,19 +105,6 @@ const getVariantsProducts = router.get("/productDetail", async (req, res) => {
     const connection = await pool.promise().getConnection();
     const { id } = req.query;
     const productId = id;
-
-    const sqlGetProductList = `SELECT p.id AS productId,
-    p.productName,
-    v.warehouseId,
-    v.id As variantId,
-    v.color as variant,
-    v.size,
-    v.price,
-    v.image,
-    v.qtyAvailable,
-    v.qtyTotal FROM products as p
-    JOIN variant as v ON v.productId = p.id
-    Where p.id = ?;`;
 
     const [result] = await connection.query(sqlGetProductList, productId);
     connection.release();
