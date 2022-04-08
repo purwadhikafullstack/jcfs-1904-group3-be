17b33ad5a84da82b6b34b81d9d23e8fdc2b9f811
FahemHakikiKhaya <hakikihaya@gmail.com>
2022-04-08 11:09:06 +0700

    new feature: unfinished multer setup

--- src/router/products/post.js
+++ src/router/products/post.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../../config/database");
+const { uploadProducts } = require("../services/multer");
+
+const uploadProductImage = uploadProducts.single("image");
+
+const postProduct = router.post("/category", async (req, res) => {
+  try {
+    const connection = await pool.promise().getConnection();
+    const { productName } = req.body;
+
+    const sqlPostProductCategory = `INSERT INTO categories_products (productId,categoryId) values(?,?);`;
+    const data = [productId, categoryId];
+    const [result] = await connection.query(sqlPostProductCategory, data);
+    connection.release();
+
+    res.status(200).send({ message: "Data telah berhasil di tambahkan" });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 const postProductCategory = router.post("/category", async (req, res) => {
   try {
@@ -17,5 +36,19 @@ const postProductCategory = router.post("/category", async (req, res) => {
     console.log(error);
   }
 });
+
+const postImageProduct = router.post(
+  "/upload",
+  uploadProductImage,
+  async (req, res) => {
+    try {
+      const sqlPostImageProduct = `UPDATE products as p
+      join variant as v on v.productId = p.id
+      SET ? Where productId = ? and v.id = ?;`;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
 module.exports = { postProductCategory };
---
+++ src/router/services/multer/index.js
@@ -0,0 +1,28 @@
+const multer = require("multer");
+const path = require("path");
+
+const photoDirectoryProducts = path.join(
+  __dirname,
+  "../../../public/images/products"
+);
+
+const storageProducts = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, photoDirectoryProducts);
+  },
+  filename: function (req, file, cb) {
+    cb(null, `test-photo.png`);
+  },
+});
+
+const uploadProducts = multer({
+  storage: storageProducts,
+  limits: {
+    filesize: 10000000,
+  },
+  fileFilter(req, file, cb) {
+    cb(null, true);
+  },
+});
+
+module.exports = { uploadProducts };
