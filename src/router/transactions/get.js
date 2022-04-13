911b5ad19f823cd5f0b04e67f1c12b4c5d8a6e27
FahemHakikiKhaya <hakikihaya@gmail.com>
2022-04-13 14:01:46 +0700

    updated feature  : added sort to chart

--- src/router/transactions/get.js
+++ src/router/transactions/get.js
@@ -6,10 +6,19 @@ const getTransactionSumPerMonth = router.get(
   async (req, res) => {
     try {
       const connection = await pool.promise().getConnection();
-      const sqlgetTransactionSumPerMonth = `select created_at as Date , sum(totalAmount) as totalAmount from transactions 
-      WHERE YEAR(created_at)=YEAR(curdate()) group by MONTH(created_at);
+      const { year, method } = req.query;
+      const addedMethodQuery = ` ${method}(created_at);`;
+      var sqlgetTransactionSumPerMonth = `select created_at as Date , sum(totalAmount) as totalAmount from transactions 
+      WHERE YEAR(created_at) = ? group by
       `;
-      const [result] = await connection.query(sqlgetTransactionSumPerMonth);
+      sqlgetTransactionSumPerMonth += addedMethodQuery;
+
+      const data = [parseInt(year)];
+      console.log(data);
+      const [result] = await connection.query(
+        sqlgetTransactionSumPerMonth,
+        data
+      );
       connection.release();
 
       res.status(200).send({ result });
