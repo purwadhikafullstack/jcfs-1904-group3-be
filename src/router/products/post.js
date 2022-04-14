b833ef8739f8d9971dc98a49b20f70d466f5e165
FahemHakikiKhaya <94028480+FahemHakikiKhaya@users.noreply.github.com>
2022-04-14 11:44:29 +0700

    Thew 4 (#4)
    * Update: Foldering
    
    * New Feature : get product distinct
    
    * new feature: get products based on Id params
    
    * new feature : getting product list by category
    
    * new feature : get products based on search keyword
    
    * new feature: backend pagination
    
    * new features : post product category, get product category, get all category
    
    * new feature : delete product category
    
    * new feature: update products
    
    * new feature: unfinished multer setup
    
    * new feature : postNewVariant (finish) and postVariantImage(unfinished need unique file multer name)
    
    * new feature : post product
    
    * updated feature : sorting using backend
    
    * new feature: get transaction sum per month
    
    * update feature : getTransactionSumPerMonth (get by current year)
    
    * updated feature  : added sort to chart
    
    * updated feature : sales report finish

--- src/router/products/post.js
+++ src/router/products/post.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../../config/database");
+const { uploadProducts } = require("../services/multer");
+
+const uploadProductImage = uploadProducts.single("image");
+
+const postProduct = router.post("/", async (req, res) => {
+  try {
+    const connection = await pool.promise().getConnection();
+    const { productName } = req.body;
+    const sqlPostProduct = `INSERT INTO products SET productName = ?`;
+
+    const [result] = await connection.query(sqlPostProduct, productName);
+    connection.release();
+
+    const sqlGetNewProduct = `SELECT id FROM products ORDER BY id DESC LIMIT 1`;
+    const [getProduct] = await connection.query(sqlGetNewProduct);
+
+    res.status(200).send(getProduct[0]);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 const postProductCategory = router.post("/category", async (req, res) => {
   try {
@@ -18,4 +39,70 @@ const postProductCategory = router.post("/category", async (req, res) => {
   }
 });
 
-module.exports = { postProductCategory };
+const postProductVariant = router.post("/variant", async (req, res) => {
+  try {
+    const connection = await pool.promise().getConnection();
+
+    const { quantity, productId, color, price, warehouseId } = req.body;
+
+    if (!req.body.size) {
+      delete req.body.size;
+    }
+
+    delete req.body.quantity;
+    delete req.body.productId;
+
+    req.body.warehouseId = parseInt(req.body.warehouseId);
+
+    const qtyTotal = quantity;
+    const qtyAvailable = quantity;
+
+    const sqlPostProductVariant = `INSERT INTO variant
+    (color, price, warehouseId, qtyTotal, qtyAvailable, productId) values(?,?,?,?,?,?);`;
+    const data = [color, price, warehouseId, qtyTotal, qtyAvailable, productId];
+
+    const [resultPost] = await connection.query(sqlPostProductVariant, data);
+
+    const sqlGetVariantsId = `SELECT id FROM variant ORDER BY id DESC LIMIT 1`;
+
+    const [resultGet] = await connection.query(sqlGetVariantsId);
+
+    res
+      .status(200)
+      .send({ message: "Data telah berhasil di tambahkan", resultGet });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const postVariantImage = router.post(
+  "/variant/image",
+  uploadProductImage,
+  async (req, res) => {
+    try {
+      const connection = await pool.promise().getConnection();
+
+      const { id } = req.body;
+      const image = `http://localhost:2023/images/products/${req.file.filename}`;
+
+      const sqlPutUserPhoto = `UPDATE variant SET ? WHERE id = ?`;
+      const dataPutUserPhoto = [{ image: image }, id];
+
+      const [result] = await connection.query(
+        sqlPutUserPhoto,
+        dataPutUserPhoto
+      );
+
+      res.status(200).send({ message: "Data telah berhasil di tambahkan" });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
+
+module.exports = {
+  postProductCategory,
+  postVariantImage,
+  postProductVariant,
+  postProduct,
+};
